50187b5d08735ea8562d895380a73a639cb53864
Malod219 <perf3ctodst@gmail.com>
2019-12-11 20:20:16 +0000

    Day 10 solutions

--- DAY_10/SOLUTION_1.py
+++ DAY_10/SOLUTION_1.py
@@ -1,31 +1,64 @@
 import math
-
+import itertools
+from collections import defaultdict
+import pprint
+# part 1 stuff
 with open("input1.txt","r") as f:
     data = f.readlines()
 points = []
-
 for y in range(len(data)):
-    for x in range(len(data[0])):
-        points.append([x,y])
-
-# Defaults
+    for x in range(len(data[0])-1):
+        if data[y][x] == '#':
+            points.append([x,y])
 maxSighted = 0
-keyPoint = [0,0]
-
+keyPoint = [0,0,-1]
 for i in range(len(points)):
     point = points[i]
-    # We will use a dict with lengths
-    # Maps ANGLE-> List[Distance, Point index]
-    visible = {}
-    #Find gradient, distance to(pythag) and point index
+    visible = []
     for j in range(len(points)):
         if i==j:
             continue
         newPoint = points[j]
-        for k in range(len(points)):
-            if k == i or k == j:
-                continue
-            newPoint2 = points[k]
-            # (y2-y1)(x3-x1)=(y3-y1)*(x2-x1)
-            grad1 = (newPoint[1] - point[1])*(newPoint2[0]-point[0])
-            grad2 = (newPoint2[1]-point[1])*(newPoint[0]-point[0])
+        angle = math.atan2(point[1]-newPoint[1],point[0]-newPoint[0])+math.pi
+        angle = math.degrees(angle)
+        if angle not in visible:
+            visible.append(angle)
+    if len(visible)>maxSighted:
+        maxSighted=len(visible)
+        keyPoint=[point[0], point[1], i]
+print("Base at {}, with sight of {} asteroids".format(keyPoint, maxSighted))
+# part 2
+roids = defaultdict(list)
+# We have to reorganise the data to consider distances too
+def getDistToPoint(px, py, px2, py2):
+    return math.sqrt((px-px2)**2 + (py-py2)**2)
+for j in range(len(points)):
+    if keyPoint[2]==j:
+        continue
+    newPoint = points[j]
+    angle = math.atan2(keyPoint[1]-newPoint[1],keyPoint[0]-newPoint[0])+(math.pi*3/2)
+    angle = math.degrees(angle)
+    if angle>= 360:
+        angle-=360
+    roids[angle].append([newPoint, getDistToPoint(keyPoint[0], keyPoint[1], newPoint[0], newPoint[1])])
+def generateKeysToIterateThrough(roids):
+    keys = []
+    for key in roids.keys():
+        keys.append(key)
+    keys.sort()
+    return keys
+destroyCount = 0
+while destroyCount<202:
+    keys = generateKeysToIterateThrough(roids)
+    for key in keys:
+        asteroidSelection = roids[key]
+        minIndex = 0
+        minDist = 99999
+        for asteroid in asteroidSelection:
+            if minDist > asteroid[1]:
+                minIndex = asteroidSelection.index(asteroid)
+                minDist = asteroid[1]
+        destroyCount+=1
+        if destroyCount == 200:
+            print(roids[key][minIndex][0][0]*100+roids[key][minIndex][0][1])
+        del roids[key][minIndex]
